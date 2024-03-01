582749ff89aeacfe73bd601aa5edce376ff5c22d
NathanDuda <nathanduda02@gmail.com>
2024-03-01 18:53:25 -0500

    initial commit

--- app.R
+++ app.R
@@ -1,9 +1,8 @@
-# Load packages ----------------------------------------------------------------
-
 library(shiny)
 library(ggplot2)
 library(dplyr)
 library(RIdeogram)
+library(Biostrings)
 
 # Load data --------------------------------------------------------------------
 
@@ -11,19 +10,21 @@ species_dnds <- read.csv("C:/Users/17735/Downloads/Visualizing_Selection/species
 species_dnds <- species_dnds %>%
   filter(dnds > 1 & dnds < 2)
 
-kar <- data.frame(
-  Chr = c('X', 2, 3, 4, 5),
-  Start = c(0, 0, 0, 0, 0),
-  End = c(248956422, 242193529, 198295559, 190214555, 181538259)
-)
+genome <- readDNAStringSet("C:/Users/17735/Downloads/GCF_000001215.4_Release_6_plus_ISO1_MT_genomic.fna")
+genome <- genome[!grepl("chromosome Y| chromosome 4| mitochondr", names(genome))]
+genome <- genome[grepl("^NT|^NC", names(genome))]
+names(genome) <- sub(".+\\s", "", names(genome))
 
-# Assuming you have data for genes and their locations
+kar <- data.frame(
+  Chr = names(genome),
+  Start = c(0, 0, 0, 0, 0),
+  End = width(genome)
+)
 
 # Define UI --------------------------------------------------------------------
 
 ui <- fluidPage(
   br(),
-  
   sidebarLayout(
     sidebarPanel(
       selectInput(
@@ -31,25 +32,25 @@ ui <- fluidPage(
         choices = c("species", "dnds"),
         selected = "species"
       ),
-      
       selectInput(
         inputId = "x", label = "X-axis:",
         choices = c("species", "dnds"),
         selected = "dnds"
-      )
+      ),
+      fileInput("hyphy_output", "HyPhy Output File:"),
+      fileInput("genome", "Genome Assembly")
     ),
-    
     mainPanel(
+      selectInput("species_select", "Select Species:", choices = NULL),
       plotOutput(outputId = "scatterplot", brush = "plot_brush"),
-      dataTableOutput(outputId = "table"),  # Changed outputId to "table"
-      plotOutput(outputId = "ideogram"),    # Changed to rideogramOutput
+      imageOutput("chromosome_png"),
+      dataTableOutput(outputId = "table"), 
       br()
     )
   )
 )
 
 # Define server ----------------------------------------------------------------
-
 server <- function(input, output, session) {
   
   output$scatterplot <- renderPlot({
@@ -58,18 +59,37 @@ server <- function(input, output, session) {
   })
   
   output$table <- renderDataTable({
-    brushedPoints(species_dnds, brush = input$plot_brush) %>% select(species, dnds)
+    brushedPoints(species_dnds, brush = input$plot_brush) %>% 
+      select(species, id, dnds, dn, ds, chrom, start, end)
   })
   
-  output$ideogram <- renderPlot({
+  observeEvent(input$plot_brush, {
     # Filter genes data based on selected points
     selected_genes <- brushedPoints(species_dnds, brush = input$plot_brush)
-    filtered_genes_data <- species_dnds %>%
-      filter(id %in% selected_genes$gene_id)  # Assuming gene_id is the identifier for genes
     
-    # Plot rideogram
-    ideogram(karyotype = kar, label = filtered_genes_data, label_type = 'marker')
-    # You may need to adjust aesthetics and other settings based on your genes_data structure
+    updateSelectInput(session, "species_select", choices = unique(selected_genes$species))
+    
+    filtered_genes_data <- species_dnds %>%
+      filter(species %in% input$species_select & 
+               id %in% selected_genes$id)
+    
+    if (nrow(filtered_genes_data) > 0) {
+      filtered_genes_data <- filtered_genes_data %>%
+        mutate(Shape = 'circle', color = 'blue') %>%
+        select(Type = species, Shape, Chr = chrom, Start = start, End = end, color)
+      
+      ideogram(karyotype = kar, label = filtered_genes_data, label_type = 'marker')
+      convertSVG("chromosome.svg", device = "png")
+      
+      output$chromosome_png <- renderUI({
+        tags$img(src = "chromosome.png", height = '600', width = "500")
+      })
+    } else {
+      # If filtered_genes_data is empty, render an empty image
+      output$chromosome_png <- renderUI({
+        tags$img(src = "chromosome.png", height = '600', width = "500")
+      })
+    }
   })
 }
 
